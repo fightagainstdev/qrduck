cb9ac073a7654dfe017d0b6e7478cd79312c269b
fightagainstdev <143303788+fightagainstdev@users.noreply.github.com>
2025-11-19 11:30:53 +0800

    stack

--- src/js/colors.js
+++
@@ -1,6 +0,0 @@
-/** @format */
-
-var Colors = {
-	white: new Color(1, 1, 1),
-	grey: new Color(0.5, 0.5, 0.5),
-};
--- ziptest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf ziptest/
-mkdir ziptest/
-cd ziptest/
-unzip ../dist/a.zip
-http-server
